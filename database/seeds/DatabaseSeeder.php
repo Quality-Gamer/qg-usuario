28242b2eeac07dd1d5b9e84a59654fb9d5153371
Agualuza <iagoagualuza@id.uff.br>
2020-07-04 15:19:57 -0300

    Adiciona novas regras ao seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,61 @@ class DatabaseSeeder extends Seeder
                 'email' => 'iagoagualuza@id.uff.br',
                 'password' => Hash::make('123456'),
             ]);
+
+            DB::table('test')->insert([
+                'match_id' => Hash::make('4353454'),
+                'title' => 'Python',
+                'test_value' => 25.00,
+                'badge' => '/python.png'
+            ]);
+
+            DB::table('test')->insert([
+                'match_id' => Hash::make('s3343'),
+                'title' => 'Go',
+                'test_value' => 25.00,
+                'badge' => '/go.png'
+            ]);
+
+            DB::table('question')->insert([
+                'test_id' => 1,
+                'order' => 0,
+                'question' => 'Py???',
+                'option_a' => 'resp a',
+                'option_b' => 'resp b',
+                'option_c' => 'resp c',
+                'option_d' => 'resp d',
+                'response' => "C",
+            ]);
+
+            DB::table('question')->insert([
+                'test_id' => 1,
+                'order' => 1,
+                'question' => 'PPlay???',
+                'option_a' => 'resp a',
+                'option_b' => 'resp b',
+                'option_c' => 'resp c',
+                'option_d' => 'resp d',
+                'response' => "B",
+            ]);
+
+            DB::table('question')->insert([
+                'test_id' => 2,
+                'order' => 0,
+                'question' => 'Gopher???',
+                'option_a' => 'resp a',
+                'option_b' => 'resp b',
+                'option_c' => 'resp c',
+                'option_d' => 'resp d',
+                'response' => "A",
+            ]);
+
+            DB::table('user_test')->insert([
+                'user_id' => 1,
+                'test_id' => 2,
+                'score' => 45.00,
+                'win' => 0,
+            ]);
+           
         }
     }
 }
