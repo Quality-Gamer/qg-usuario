2100a7295352dcdd68804c52e4b3053fb5a960ca
Agualuza <iagoagualuza@id.uff.br>
2020-07-12 12:56:39 -0300

    Update TestsController.php

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -50,7 +50,7 @@ class TestsController extends Controller
 
         $userTest = UserTest::where('match_id', $match_id)->first();
         
-        if(!isset($userTest)){
+        if(!isset($userTest) || empty($userTest)){
             return APIService::sendJson(["status" => "NOK", "response" => NULL, "message" => "Esse teste não foi iniciado"]);
         }
 
@@ -81,7 +81,7 @@ class TestsController extends Controller
         $win = $request->input('win');
         $ut = UserTest::where('match_id', $match_id)->first();;
         
-        if(!isset($ut)){
+        if(!isset($ut) || empty($ut)){
             $win = null;
             $ut = new UserTest();
         }
