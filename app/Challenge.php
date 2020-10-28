3ae04454c7d9b89fdcd6a4c077689407b798d8f0
Agualuza <iagoagualuza@id.uff.br>
2020-10-28 04:04:03 +0000

    Bug fix

--- app/Challenge.php
+++ app/Challenge.php
@@ -10,7 +10,7 @@ class Challenge extends Model
     protected $table = 'challenge';
     public static function loadAllChallangesByUser($uid) {
         return DB::table('challenge')
-                ->join('user_challenge', 'challenge.id', '=', 'user_challenge.challange_id')
+                ->join('user_challenge', 'challenge.id', '=', 'user_challenge.challenge_id')
                 ->whereRaw('user_challenge.user_id = ? and challenge.active = 1', [$uid])
                 ->get();
 
