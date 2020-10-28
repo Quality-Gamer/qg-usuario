a38050c2704f231d50af7894d3d42ddcb3af6d08
Agualuza <iagoagualuza@id.uff.br>
2020-10-28 03:45:58 +0000

    Retorna apenas desafios ativos

--- app/Challenge.php
+++ app/Challenge.php
@@ -10,7 +10,7 @@ class Challenge extends Model
     public static function loadAllChallangesByUser($uid) {
         return DB::table('challenge')
                 ->join('user_challenge', 'challenge.id', '=', 'user_challenge.challange_id')
-                ->whereRaw('user_challenge.user_id = ?', [$uid])
+                ->whereRaw('user_challenge.user_id = ? and challenge.active = 1', [$uid])
                 ->get();
 
     }
