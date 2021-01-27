072c2f8180f519962d288de9301ab6b006b42afb
Agualuza <iagoagualuza@id.uff.br>
2021-01-26 23:51:45 -0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,7 +45,7 @@ class UserController extends Controller
         $user->char_id = $request->input('char');
 
         $exists = DB::select('select count(1) as _exists from user where email = ?', [$user->email]);
-        return $exists[0]["_exists"];
+        return $exists[0]->_exists;
         if($exists[0]->_exists) {
             return APIService::sendJson(["status" => "NOK", "response" => [], "message" => "Email já registrado"]);
         }
